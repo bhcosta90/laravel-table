5e516c731780d1278a32078c8a43b44af2ee5ef1
Bruno Henrique da Costa <bhcosta90@gmail.com>
2021-07-29 12:59:24 -0300

    Update table.blade.php

--- resources/views/table.blade.php
+++ resources/views/table.blade.php
@@ -1,53 +1,53 @@
 @if(count($rows) > 0)
-<table class="{{ $class ?? 'table' }}">
-    @if(count($columns))
-	<thead>
-		<tr>
-        @foreach($columns as $c)
-            <th {!! $c->getClasses() ? ' class="' . $c->getClassString() . '"' : '' !!}>
-                @if($c->isSortable())
-                    <a href="{{ $c->getSortURL() }}">
-                        {!! $c->getLabel() !!}
-                        @if($c->isSorted())
-                            @if($c->getDirection() == 'asc')
-                                <span class="fa fa-sort-asc"></span>
-                            @elseif($c->getDirection() == 'desc')
-                                <span class="fa fa-sort-desc"></span>
-                            @endif
-                        @endif
-                    </a>
-                @else
-                    {{ $c->getLabel() }}
-                @endif
-            </th>
-        @endforeach
-
-		</tr>
-	</thead>
-    @endif
-	<tbody>
-        @if(count($rows))
-            @foreach($rows as $r)
-
-        <tr>
+    <table class="{{ $class ?? 'table' }}">
+        @if(count($columns))
+        <thead>
+            <tr>
             @foreach($columns as $c)
-                <td {!! $c->getClasses() ? ' class="' . $c->getClassString() . '"' : '' !!}>
-                 @if($c->hasRenderer())
-                    {{-- Custom renderer applied to this column, call it now --}}
-                    {!! $c->render($r) !!}
+                <th {!! $c->getClasses() ? ' class="' . $c->getClassString() . '"' : '' !!}>
+                    @if($c->isSortable())
+                        <a href="{{ $c->getSortURL() }}">
+                            {!! $c->getLabel() !!}
+                            @if($c->isSorted())
+                                @if($c->getDirection() == 'asc')
+                                    <span class="fa fa-sort-asc"></span>
+                                @elseif($c->getDirection() == 'desc')
+                                    <span class="fa fa-sort-desc"></span>
+                                @endif
+                            @endif
+                        </a>
                     @else
-                    {{-- Use the "rendered_foo" field, if available, else use the plain "foo" field --}}
-                        {!! $r->{'rendered_' . $c->getField()} ?? $r->{$c->getField()} !!}
+                        {{ $c->getLabel() }}
                     @endif
-                </td>
+                </th>
             @endforeach
 
-        </tr>
-
-            @endforeach
+            </tr>
+        </thead>
         @endif
-	</tbody>
-</table>
+        <tbody>
+            @if(count($rows))
+                @foreach($rows as $r)
+
+            <tr>
+                @foreach($columns as $c)
+                    <td {!! $c->getClasses() ? ' class="' . $c->getClassString() . '"' : '' !!}>
+                    @if($c->hasRenderer())
+                        {{-- Custom renderer applied to this column, call it now --}}
+                        {!! $c->render($r) !!}
+                        @else
+                        {{-- Use the "rendered_foo" field, if available, else use the plain "foo" field --}}
+                            {!! $r->{'rendered_' . $c->getField()} ?? $r->{$c->getField()} !!}
+                        @endif
+                    </td>
+                @endforeach
+
+            </tr>
+
+                @endforeach
+            @endif
+        </tbody>
+    </table>
 @else
     <div class='alert alert-info text-center'>{{ __('Do not results') }}</div>
 @endif
